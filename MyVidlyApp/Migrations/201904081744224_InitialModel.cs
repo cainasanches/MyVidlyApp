b136027cb8f987590088e76a29b1b2ad1a903eb6
Cainã Assis Sanches <cainasanches@hotmail.com>
2019-04-08 15:03:22 -0300

    Utilizando Entity

---
+++ MyVidlyApp/Migrations/201904081744224_InitialModel.cs
@@ -0,0 +1,31 @@
+namespace MyVidlyApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Customer_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Customers", t => t.Customer_Id)
+                .Index(t => t.Customer_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Customers", "Customer_Id", "dbo.Customers");
+            DropIndex("dbo.Customers", new[] { "Customer_Id" });
+            DropTable("dbo.Customers");
+        }
+    }
+}
---
+++ MyVidlyApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace MyVidlyApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public ApplicationDbContext()
+        {
+
+        }
+    }
+}
